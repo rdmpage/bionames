ba14b6a10be5c11ecd95866660d3b423d04d8703
Roderic Page <rdmpage@gmail.com>
2014-08-19 15:11:02 +0100

    Support for direct linking using identifier, e.g. DOI

---
+++ display_identifier.php
@@ -0,0 +1,69 @@
+<?php
+
+require_once('bionames-api/lib.php');
+
+
+// Look up record that corresponds to identifer 
+
+// mockup template
+
+// do PHP stuff here to get query parameters...
+
+/*
+echo '<pre>';
+print_r($_GET);
+echo '</pre>';
+*/
+
+//exit(0);
+
+$id = $_GET['id'];
+$namespace = $_GET['namespace'];
+
+// OK, we need some HTML content that Google can see when it crawls the page...
+$url = "http://bionames.org/api/api_id.php?id=" . urlencode($id) . "&namespace=" . $namespace;
+
+
+$json = get($url);
+
+
+if ($json != '')
+{
+	$obj = json_decode($json);
+	
+	//echo $obj->_id;
+	
+	switch ($obj->type)
+	{
+		case 'article':
+		case 'book':
+		case 'generic':
+		case 'chapter':
+			$target = 'references/' . $obj->_id;
+			break;
+			
+		case 'nameCluster':
+			$target = 'names/' . $obj->_id;
+			break;
+
+		default:
+			$target = $obj->_id;
+			break;
+	}
+	
+	//echo $target;
+	
+	// redirect
+	header("Location: http://bionames.org/" . $target);
+	exit(0);
+}
+else
+{
+	// don't have this
+	//echo 'Not found';
+	header('HTTP/1.1 404 Not Found');	
+}
+
+
+
+?>
