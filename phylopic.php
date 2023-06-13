006f54b032e0455b972e7a7cdb0cd41e59d81df6
Roderic Page <rdmpage@gmail.com>
2023-06-13 08:35:03 +0100

    Update to phylopic v2 API

--- phylopic.php
+++ phylopic.php
@@ -12,59 +12,61 @@ if (isset($_GET['name']))
 
 $image_url = "";
 
-// name search 
-$url = 'http://phylopic.org/api/a/name/search' . '?text=' . urlencode($name);
-
-$json = get($url);
-if ($json != '')
+if (1)
 {
-	$obj = json_decode($json);
-	if (count($obj->result) > 0)
+	$prefix = substr($name, 0, 1);	
+	
+	$url = 'https://raw.githubusercontent.com/rdmpage/phylopic-taxa/master/images/' . $prefix . '/' . str_replace(' ', '%20', $name) . '.png';		
+
+	$ch = curl_init();
+	curl_setopt($ch, CURLOPT_URL, $url);
+	curl_setopt($ch, CURLOPT_HEADER, 0);
+	curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
+	curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0); 
+	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0); 
+		  	
+	$response = curl_exec($ch);
+	if($response == FALSE) 
 	{
-		$uid = $obj->result[0]->canonicalName->uid;
+		$errorText = curl_error($ch);
+		curl_close($ch);
+		die($errorText);
+	}
+	
+	$info = curl_getinfo($ch);
+	
+	$http_code = $info['http_code'];
+	
+	curl_close($ch);
 		
-		// Comment out SVG for now as we will need to be cleverer
-		// in rescaling image for display on web page, it's not as simple as PNG
-		// http://stackoverflow.com/questions/3120739/resizing-svg-in-html
-		
-		/*
-		// Try for SVG first
-		$url = 'http://phylopic.org/api/a/name/' . $uid . '/images?options=svgFile';
-		$json = get($url);
-		
+	if ($http_code == 200)
+	{
+		$image_url = $url;
+	}
+}
+
+if (0)
+{
+
+	// name search 
+	$url = 'http://phylopic.org/api/a/name/search' . '?text=' . urlencode($name);
+
+	$json = get($url);
+	if ($json != '')
+	{
 		$obj = json_decode($json);
-		
-		//print_r($obj);
-		
-		if ($image_url == '')
+		if (count($obj->result) > 0)
 		{
-			if (count($obj->result->same) > 0)
-			{
-				if (isset($obj->result->same[0]->svgFile))
-				{
-					$image_url = 'http://phylopic.org' . $obj->result->same[0]->svgFile->url;
-				}
-			}
-		}
+			$uid = $obj->result[0]->canonicalName->uid;
 		
-		if ($image_url == '')
-		{
-			if (count($obj->result->supertaxa) > 0)
-			{
-				if (isset($obj->result->supertaxa[0]->svgFile))
-				{
-					$image_url = 'http://phylopic.org' . $obj->result->supertaxa[0]->svgFile->url;
-				}
-			}
-		}
-		*/
+			// Comment out SVG for now as we will need to be cleverer
+			// in rescaling image for display on web page, it's not as simple as PNG
+			// http://stackoverflow.com/questions/3120739/resizing-svg-in-html
 		
-		// No SVG, try PNG
-		if ($image_url == '')
-		{
-		
-			$url = 'http://phylopic.org/api/a/name/' . $uid . '/images?options=pngFiles';
-		
+			/*
+			// Try for SVG first
+			$url = 'http://phylopic.org/api/a/name/' . $uid . '/images?options=svgFile';
 			$json = get($url);
 		
 			$obj = json_decode($json);
@@ -75,7 +77,10 @@ if ($json != '')
 			{
 				if (count($obj->result->same) > 0)
 				{
-					$image_url = 'http://phylopic.org' . $obj->result->same[0]->pngFiles[0]->url;
+					if (isset($obj->result->same[0]->svgFile))
+					{
+						$image_url = 'http://phylopic.org' . $obj->result->same[0]->svgFile->url;
+					}
 				}
 			}
 		
@@ -83,16 +88,49 @@ if ($json != '')
 			{
 				if (count($obj->result->supertaxa) > 0)
 				{
-					$image_url = 'http://phylopic.org' . $obj->result->supertaxa[0]->pngFiles[0]->url;
+					if (isset($obj->result->supertaxa[0]->svgFile))
+					{
+						$image_url = 'http://phylopic.org' . $obj->result->supertaxa[0]->svgFile->url;
+					}
 				}
 			}
-		}		
+			*/
+		
+			// No SVG, try PNG
+			if ($image_url == '')
+			{
+		
+				$url = 'http://phylopic.org/api/a/name/' . $uid . '/images?options=pngFiles';
+		
+				$json = get($url);
+		
+				$obj = json_decode($json);
+		
+				//print_r($obj);
+		
+				if ($image_url == '')
+				{
+					if (count($obj->result->same) > 0)
+					{
+						$image_url = 'http://phylopic.org' . $obj->result->same[0]->pngFiles[0]->url;
+					}
+				}
+		
+				if ($image_url == '')
+				{
+					if (count($obj->result->supertaxa) > 0)
+					{
+						$image_url = 'http://phylopic.org' . $obj->result->supertaxa[0]->pngFiles[0]->url;
+					}
+				}
+			}		
+		}
 	}
-}
 
-if ($image_url == '')
-{
-	$image_url = "images/1x1.png";
+	if ($image_url == '')
+	{
+		$image_url = "images/1x1.png";
+	}
 }
 
 //echo $image_url;
